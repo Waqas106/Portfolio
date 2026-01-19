e6a9518c4b15b45ae696f2fd9a4bd41c39191862
Waqas106 <m.waqas987321@gmail.com>
2026-01-19 19:08:03 +0500

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import About from "@/components/about";
-import Contact from "@/components/contact";
-import Experience from "@/components/experience";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import MainSection from "@/components/main";
-import Projects from "@/components/projects";
-import Skills from "@/components/skills";
+import About from "@/components/about.js";
+import Contact from "@/components/contact.js";
+import Experience from "@/components/experience.js";
+import Footer from "@/components/footer.js";
+import Header from "@/components/header.js";
+import MainSection from "@/components/main.js";
+import Projects from "@/components/projects.js";
+import Skills from "@/components/skills.js";
 
 export default function Home() {
   return (
